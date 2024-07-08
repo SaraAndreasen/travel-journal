1f2c40f1b52ad554ff4efbf0d1dcd56103cf512a
SaraAndreasen <55578265+SaraAndreasen@users.noreply.github.com>
2024-07-09 00:19:22 +0200

    update project, add fontawesome, finish navbar

--- src/app/ui/TopNavigation.tsx
+++ src/app/ui/TopNavigation.tsx
@@ -1,16 +1,22 @@
-import NavLinks from "./nav-links";
+"use client";
+
 import styles from "./Home.module.css";
 import GlobetrotterGramLogo from "./globetrotter-gram-logo";
+import NavLinksDesktop from "./nav-links-desktop";
+import { NavLinksMobile } from "./nav-links-mobile";
 
 export default function TopNavigation() {
   return (
     <>
       <nav className={styles.nav}>
-        <div className="logo align-baseline p-1.5">
+        <div className="logo align-baseline">
           <GlobetrotterGramLogo />
         </div>
-        <div className="nav-links">
-          <NavLinks />
+        <div className="hidden md:block mt-3 align-center">
+          <NavLinksDesktop />
+        </div>
+        <div className="md:hidden">
+          <NavLinksMobile />
         </div>
       </nav>
     </>
---
+++ src/app/ui/nav-links-desktop.tsx
@@ -0,0 +1,11 @@
+export default function NavLinksDesktop() {
+  return (
+    <>
+      <div className="flex justify-around">
+        <p className="p-1.5">login desktop</p>
+        <p className="p-1.5">your travels</p>
+        <p className="p-1.5">friends</p>
+      </div>
+    </>
+  );
+}
