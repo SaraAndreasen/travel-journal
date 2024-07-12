0330c684a515f39da40fe4055845c0d26b654ad9
SaraAndreasen <55578265+SaraAndreasen@users.noreply.github.com>
2024-07-12 23:02:13 +0200

    herobanner completed

--- src/app/ui/home/herobanner.tsx
+++ src/app/ui/home/herobanner.tsx
@@ -2,14 +2,18 @@ import styles from "../Home.module.css";
 
 export function Herobanner() {
   return (
-    <div className={`${styles.herobannerBackground} p-32`}>
-      <div className="bg-white bg-opacity-70 rounded-2xl p-24">
-        <h1 className="text-center text-5xl p-8">
+    <div className={`${styles.herobannerBackground}`}>
+      <div className="bg-white bg-opacity-70 p-24 md:p-32">
+        <h1 className="sm:text-left md:text-center text-5xl pb-10">
           Your digital travel journal
         </h1>
-        <button className={`${styles.heroButton} text-white p-2.5 rounded-2xl`}>
-          Create your own journal
-        </button>
+        <div className="flex sm:justify-start md:justify-center">
+          <button
+            className={`${styles.heroButton} text-white p-2.5 rounded-2xl md:mt-10`}
+          >
+            Create your own journal
+          </button>
+        </div>
       </div>
     </div>
   );
